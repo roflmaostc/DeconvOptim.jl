927ae6083ea9f76bd150127f6675dbebb8d0e42b
roflmaostc <f.wechsler@tum.de>
2020-06-21 19:38:45 +0200

    Some metaprogramming for generating regularizers

--- src/regularizer.jl
+++ src/regularizer.jl
@@ -1,53 +1,151 @@
-using Zygote: @adjoint, gradient
-using Statistics
-using Distributed
+using Zygote
+using Tullio
 
 export GR, GR_old
 export Tikhonov_old, Tikhonov
 export TV, TV_old
 export Positivity
 export laplace
+export create_Ndim_regularizer
+export lol
+export generate_spatial_grad_square_n
 
  # First we define several functions which are helpful
  # to use in some of the regularizers
 function laplace(rec) 
+    # todo proper handling for 3D and 2D because laplacian
+    # is defined differently
     @tullio res = (rec[i - 1, j, k, l, m] + rec[i + 1, j, k, l, m] +   
-                   rec[i, j + 1, k, l, m] + rec[i, j - 1, k, l, m] - 4 * rec[i,j])^2
+                   rec[i, j - 1, k, l, m] + rec[i, j + 1, k, l, m] +   
+                   rec[i, j, k - 1, l, m] + rec[i, j, k + 1, l, m] - 
+                   4 * rec[i,j])^2
     return res
 end
 
 
-function spatial_grad_square(rec)
-    @tullio res = ((rec[i - 1, j, k, l, m] - rec[i + 1, j, k, l, m])^2 +
-                   (rec[i, j - 1, k, l, m] - rec[i, j + 1, k, l, m])^2)
-    return res
+ # meta programing
+ # Reference and thanks to @mcabbott
+ # https://github.com/mcabbott/Tullio.jl/issues/11
+ #
+ # this function creates n dimensional Tullio functions for calculating
+ # some regularizers
+ # expr is the parameter which performs the operation of the regularizer
+ # comb_f is a function combinining these elementary operations over
+ #      dimensions
+ # off1 and off2 are offsets of the filter
+ #
+function create_Ndim_regularizer(expr, comb_f, Ndim;off1=1, off2=-1)
+    out, add = [], []
+    for d in 1:Ndim
+        ind = :i # @gensym ind                                                  
+        inds1 = map(1:Ndim) do di                                         
+            i = Symbol(ind, di)                                                 
+            di == d ? :($i + $off1) : i                                               
+        end                                                                     
+        inds2 = map(1:Ndim) do di                                         
+            i = Symbol(ind, di)                                                 
+            di == d ? :($i + $off2) : i                                               
+        end                                                                     
+        res = Symbol(:res_,d) # @gensym res                                     
+        push!(out, :($res = let
+            @tullio $res = $(expr(d, inds1, inds2))
+        end))                                                                   
+        push!(add, res)                                                         
+    end                                                                         
+    push!(out, :($comb_f(+($(add...)))))
+    return out
 end
 
+function create_Ndim_regularizer2(expr, comb_f, num_dim, sum_dim)
+    out, add = [], []
+    for d in 1:sum_dim
+        ind = :i # @gensym ind
+        inds1 = map(1:num_dim) do di
+            i = Symbol(ind, di)
+            di == d ? :($i+1) : i
+        end
+        inds2 = map(1:num_dim) do di
+            i = Symbol(ind, di)
+            di == d ? :($i-1) : i
+        end
+        push!(add, expr(inds1, inds2))
+    end
+    if comb_f == nothing
+        push!(out, :(@tullio res = +($(add...))))
+    else
+        push!(out, :(@tullio res = $comb_f(+($(add...)))))
+    end
+    return out
+end
+
+function generate_spatial_grad_square_n(num_dim, sum_dim)
+    expr(inds1, inds2) = :(arr[$(inds1...)] - arr[$(inds2...)])
+    comb_f = nothing#:abs2 
+    @eval x = arr -> ($(create_Ndim_regularizer2(expr, comb_f,
+                                                 num_dim, sum_dim)...))
+    return x
+end
+
+ # defining the spatial grad_regularizer for different dimensions 
+for N in 1:10
+expr(d, inds1, inds2) = :(weights[$d] * abs2(arr[$(inds1...)] 
+                                           - arr[$(inds2...)]))
+    @eval function spatial_grad_square(arr::AbstractArray{T,$N}; 
+            weights=nothing) where T
+        if weights == nothing
+            weights = ones(ndims(arr))
+        end
+        $(create_Ndim_regularizer(expr, identity, N)...)
+    end
+end
+
+
+
+
+ # quadrat für rotationsinvarianz
+ # manhatten norm
 function forward_gradient(rec)
-    @tullio res = rec[i + 1, j, k, l, m] - rec[i, j, k, l, m] 
-    return res 
-end
-
-function backward_gradient(rec)
-    @tullio res = rec[i, j, k, l, m] - rec[i - 1, j, k, l, m]  
-    return res 
+    res1 = let 
+        @tullio res1 = abs2(rec[i + 1, j, k, l, m] - rec[i, j, k, l, m])
+    end
+    res2 = let 
+        @tullio res2 = abs2(rec[i, j + 1, k, l, m] - rec[i, j, k, l, m]) 
+    end
+    res3 = let 
+        @tullio res3 = abs2(rec[i, j, k + 1, l, m] - rec[i, j, k, l, m])
+    end
+    return res1 + res2 + res3
 end
 
 
 function central_gradient(rec)
-    @tullio res = 0.5 * (rec[i + 1, j, k, l, m] - rec[i - 1, j, k, l, m])  
+    @tullio res = 0.5 * (abs2(rec[i - 1, j, k, l, m] - rec[i + 1, j, k, l, m]) +  
+                         abs2(rec[i, j - 1, k, l, m] - rec[i, j + 1, k, l, m]) + 
+                         abs2(rec[i, j, k - 1, l, m] - rec[i, j, k + 1, l, m])) 
     return res 
 end
 
 
-function Positivity(; λ=0.05)
+# Tikhonov is the preferred implementation over Tikhonov_old
+function Tikhonov(; λ=0.05, mode="laplace")
+
+    if mode == "laplace"
+        Γ = laplace
+    elseif mode == "spatial_grad_square"
+        Γ = generate_spatial_grad_square_n(5,2)#spatial_grad_square
+    elseif mode == "forward_grad"
+        Γ = forward_gradient 
+    elseif mode == "central_grad"
+        Γ = central_gradient 
+    end
+
     function f!(F, G, rec)
         if G != nothing
-            G .= λ .* min.(0, rec) .* 2 ./ length(rec)
+            G .= λ .* gradient(Γ, rec)[1] / length(rec)
         end
 
         if F != nothing
-            return λ * sum(min.(0, rec) .^ 2) / length(rec)
+            return λ * Γ(rec) / length(rec)
         end
     end
     return f!
@@ -55,16 +153,20 @@ end
 
 
 
+ # √(abs2(rec) + ϵ)
+ # x²  
 function GR(; λ=0.05, mode="central", ϵ=1e-5)
     function GR_central(rec)
         @tullio reg = ((rec[i - 1, j, k, l, m] - rec[i + 1, j, k, l, m])^2 + 
-                       (rec[i, j - 1, k, l, m] - rec[i, j + 1, k, l, m])^2) / 
+                       (rec[i, j - 1, k, l, m] - rec[i, j + 1, k, l, m])^2 + 
+                       (rec[i, j, k - 1, l, m] - rec[i, j, k + 1, l, m])^2) / 
                       (rec[i, j, k, l, m] + ϵ)
         return reg 
     end
     function GR_forward(rec)
         @tullio reg = ((rec[i, j, k, l, m] - rec[i + 1, j, k, l, m])^2 + 
-                       (rec[i, j, k, l, m] - rec[i, j + 1, k, l, m])^2) / 
+                       (rec[i, j, k, l, m] - rec[i, j + 1, k, l, m])^2 + 
+                       (rec[i, j, k, l, m] - rec[i, j, k + 1, l, m])^2) / 
                       (rec[i, j, k, l, m] + ϵ)
         return reg
     end
@@ -87,41 +189,19 @@ function GR(; λ=0.05, mode="central", ϵ=1e-5)
 end
 
 
-# Tikhonov is the preferred implementation over Tikhonov_old
-function Tikhonov(; λ=0.05, mode="laplace")
-
-    if mode == "laplace"
-        Γ = laplace
-    elseif mode == "spatial_grad_square"
-        Γ = spatial_grad_square
-    elseif mode == "forward_grad"
-        Γ = forward_gradient 
-    elseif mode == "central_grad"
-        Γ = central_gradient 
-    end
-
-    function f!(F, G, rec)
-        if G != nothing
-            G .= λ .* gradient(Γ, rec)[1] / length(rec)
-        end
-
-        if F != nothing
-            return λ * Γ(rec) / length(rec)
-        end
-    end
-    return f!
-end
 
 function TV(; λ=0.05, mode="central")
     function total_var_center(rec)
         @tullio reg = sqrt((rec[i - 1, j, k, l, m] - rec[i + 1, j, k, l, m])^2 + 
-                           (rec[i, j - 1, k, l, m] - rec[i, j + 1, k, l, m])^2)
+                           (rec[i, j - 1, k, l, m] - rec[i, j + 1, k, l, m])^2 +
+                           (rec[i, j, k - 1, l, m] - rec[i, j, k + 1, l, m])^2)
         return reg 
     end
 
     function total_var_forward(rec)
         @tullio reg = sqrt((rec[i, j, k, l, m] - rec[i + 1, j, k, l, m])^2 + 
-                           (rec[i, j, k, l, m] - rec[i, j + 1, k, l, m])^2)
+                           (rec[i, j, k, l, m] - rec[i, j + 1, k, l, m])^2 +
+                           (rec[i, j, k, l, m] - rec[i, j, k + 1, l, m])^2)
         return reg
     end
 
@@ -145,6 +225,22 @@ function TV(; λ=0.05, mode="central")
     
     return f!
 end
+
+
+function Positivity(; λ=0.05)
+    function f!(F, G, rec)
+        if G != nothing
+            G .= λ .* min.(0, rec) .* 2 ./ length(rec)
+        end
+
+        if F != nothing
+            return λ * sum(min.(0, rec) .^ 2) / length(rec)
+        end
+    end
+    return f!
+end
+
+
 
 
 
