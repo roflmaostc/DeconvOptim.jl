47252ac82fdd69c3d453782c175c2dac8208f7eb
roflmaostc <f.wechsler@tum.de>
2020-10-12 20:57:42 +0200

    Rewrite of base code

--- src/DeconvOptim.jl
+++ src/DeconvOptim.jl
@@ -7,6 +7,9 @@ using Statistics
 using FFTW
 using Distributed
 using Tullio
+using Images
+using Interpolations
+
 
 export deconvolution
 
@@ -31,7 +34,13 @@ regularizers and mappings.
                   is `GR()`. 
 - `mappingf`: Applies a mapping of the optimizer weight. Default is a 
               parabola which achieves a non-negativity constraint.
-
+- `iterations`: Specifies a number of iterations after the optimization
+    definitely should stop.
+- `options`: Can be a options file required by Optim.jl. 
+    Will overwrite iterations.
+- `plan_fft`: Boolean whether plan_fft is used. Default `true`.
+- `up_sampling` enables up sampling of the reconstruction. Needs to be an
+    integer number. Default is 1.
 """
 function deconvolution(measured::AbstractArray{T, N}, psf;
         lossf=Poisson(),
@@ -39,78 +48,126 @@ function deconvolution(measured::AbstractArray{T, N}, psf;
         mappingf=Non_negative(),
         iterations=50,
         options=nothing,
-        plan_fft=true) where {T, N}
+        plan_fft=true,
+        padding=true,
+        up_sampling=1) where {T, N}
 
-    if N == 2
-        new_size = (size(measured)..., 1, 1, 1)
-        measured = reshape(measured, new_size)
-        psf = reshape(psf, new_size) 
-    elseif N == 3 
-        new_size = (size(measured)..., 1, 1)
-        measured = reshape(measured, new_size)
-        psf = reshape(psf, new_size) 
-    end
+    # rec0 will the array storing the final reconstruction
+    # we choose it larger than the measured array to reduce
+    # wrap around artifacts of the Fourier Transform
 
-    if plan_fft
-        P = plan_rfft(measured, [1, 2, 3])
-        measured_fft = P * measured
-        P_inv = plan_irfft(measured_fft, size(measured)[1], [1, 2, 3])
-        conv(rec, otf) = conv_real_otf_p(P, P_inv, rec, otf)
-    else
-        conv = conv_real_otf
-    end
+    # we create a array size_padded which stores a new array size
+    # our reconstruction array will be larger than measured
+    # to prevent wrap around artifacts
 
- 
-    # parsing of arguments
-    if mappingf != nothing
-        mf, ∇mf, m_invf = mappingf
-    else
-        mf, ∇mf, m_invf = identity, identity, identity
-    end
-
-    if regularizerf == nothing
-        function n_regularizerf(F, G, rec)
-            if G != nothing
-                G = 0
+    size_padded = []
+    for i = 1:5
+        # if measured is smaller than i-th dimensional
+        # simply add size 1 as this dimension 
+        # if the size of the i-th dimension is 1
+        # don't do any padding because there won't be no
+        # convolution happening in that dimension
+        if ndims(measured) < i || size(measured)[i] == 1
+            push!(size_padded, 1)
+        else
+            # only pad, if padding is true
+            if padding
+                # either add a total of 20% to each dimension
+                # or add 5, if 20% is just too few   
+                x = max(5, round(Int, size(measured)[i] * 0.2))
+            else
+                x = 0
             end
-            if F != nothing
-                return 0
-            end
+            push!(size_padded, size(measured)[i] + x)
         end
-        regularizerf = n_regularizerf
     end
+    # create rec0 which will be the initial guess for the reconstruction
+    rec0 = ones(T, (size_padded...)) * mean(measured)
+
+
+#    if ndims(psf) < 5
+#        # create a new tuple containing the sizes of psf
+#        n_size_psf = (size(psf)..., ones(Integer, 5 - ndims(psf))...) 
+#        psf = reshape(psf, n_size_psf)
+#    end
 
     # transform the PSF to OTF, is more efficient
-    otf = rfft(psf)
+    # it is important to do the FFT of the 0-padded OTF
+    # Wrong is: doing the FFT first and then padding the OTF with 0s
+    #           will lead to artifacts
 
-    # lossf needs otf and measured, but only once, therefore
-    # an extra anonymous function
-    lossf2(F, G, rec) = lossf(conv, F, G, rec, otf, measured)
 
+    # do some reshaping if the data is not provided
+    # as a 5D array
+    if (N == 2 && ndims(psf) == 2)|| (N == 3 && ndims(psf) == 3)
+        new_size = (size(measured)..., ones(Integer, 5 - N)...)
+        measured = reshape(measured, new_size)
+        psf = reshape(psf, new_size)
+        fft_dims = collect(1:N) # [1,2,3] or [1,2]
+    elseif N == 4 && ndims(psf) == 3 
+        measured = reshape(measured, (size(measured)..., 1))
+        psf = reshape(psf, (size(psf)..., 1, 1))
+        fft_dims = [1, 2, 3] 
+    else                       
+        throw("Such a combination of the dimensions of PSF and Image
+               is not supported at the moment.")
+    end
+
+
+    # psf_n is the psf with the same size as rec0
+    # we put the small psf into the new one
+    psf_n = zeros(Float32, size(rec0))
+    psf_n = center_set!(psf_n, fftshift(psf))
+    psf = ifftshift(psf_n)
+
+    # use plan_fft (function of the FFTW.jl has the same name)
+    # for speed improvement
+    if plan_fft
+        # otf is obtained by rfft(psf) and therefore 
+        # size(psf)[1:3] != size(rec0)[1:3]
+        otf, conv = plan_conv_r(psf, rec0, fft_dims) 
+    else
+        otf = rfft(psf, fft_dims)
+        conv = conv_otf_r
+    end
+    
+    # we are running into a world age counter problem
+    # https://docs.julialang.org/en/v1/manual/methods/#Redefining-Methods
+    if up_sampling != 1
+        throw("up_sampling is not supported at the moment")
+        downsample = generate_downsample(5, 2, up_sampling)
+        # fix according to documentation
+        #= Base.invokelatest(downsample) =#
+    end
+ 
+    # Get the mapping functions to achieve constraints
+    # like non negativity
+    if mappingf != nothing
+        mf, m_invf = mappingf
+    else
+        mf, m_invf = identity, identity
+    end
+
+    # if no regularizer is provided, simply use x -> 0
+    if regularizerf == nothing
+        regularizerf = x -> 0 
+    end
+
+    # create the loss function which depends simply on the x
+    loss(x) = (poisson_aux(conv, mf(x), otf, measured, 1) + regularizerf(mf(x)))
+    
+    
+    
     # this is the function which will be provided to Optimize
-    # we use the trick and share computations
+    # check Optims documentation for the purpose of F and Get
+    # but simply speaking F is the loss value and G it's gradient
+    # depending whether one of them is nothing, we skip some computations
     function f!(F, G, rec)
-        # map the reconstruction according to a function to a map function
-        # can be a mapping to positive numbers for example
-        m_rec = mf(rec)
-
-
-        # if gradient should be calculated, we must allocate some space
-        # for the gradient of the regularizer
         if G != nothing
-            ∇reg = zeros(size(measured))
-        end
-
-        # calculate the regularizer loss
-        reg = regularizerf(F, ∇reg, m_rec)
-        # calculate the loss
-        loss = lossf2(F, G, m_rec)
-
-        if G != nothing
-            G .= ∇mf(rec) .* (G .+ ∇reg)
+            G .= gradient(loss, rec)[1]
         end
         if F != nothing
-            return (loss .+ reg)
+            return loss(rec)
         end
     end
 
@@ -118,13 +175,15 @@ function deconvolution(measured::AbstractArray{T, N}, psf;
     if options == nothing
         options = Optim.Options(iterations=iterations)
     end
-
-    rec0 = m_invf(conv(measured, otf))
+    
+    # do the optimization with LBGFS
     res = Optim.optimize(Optim.only_fg!(f!), rec0, LBFGS(), options)
 
-    return mf(Optim.minimizer(res)), res, rec0
-
+    # since we do some padding we need to extract the core part
+    # also apply the mapping
+    res_out = center_extract(mf(Optim.minimizer(res)), size(measured))    
+    return res_out, res
 end
 
-# Module
+# end module
 end
