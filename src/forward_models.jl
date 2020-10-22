82b295e40b18b28240ae420738556567b7e22c0f
roflmaostc <f.wechsler@tum.de>
2020-10-22 14:46:47 +0200

    Add docstrings to forward model

--- src/forward_models.jl
+++ src/forward_models.jl
@@ -1,19 +1,22 @@
-export conv_aux
+"""
+    conv_aux(conv_otf, rec, otf)
 
-function conv_aux(conv, rec, otf)
-    return conv(rec, otf)
+Calculate the convolution between `rec` and `otf`.
+The used convolution function is `conv_otf`.
+`conv_otf` can be exchanged to be a rfft, fft or plan_fft based routine.
+
+This function is just defined to speed up automatic differentiation
+and it's custom defined adjoint.
+"""
+function conv_aux(conv_otf, rec, otf)
+    return conv_otf(rec, otf)
 end
 
-
+ # define custom adjoint for conv_aux
 function ChainRulesCore.rrule(::typeof(conv_aux), conv, rec, otf)
     Y = conv_aux(conv, rec, otf)
     function conv_aux_pullback(barx)
-        return NO_FIELDS, DoesNotExist(), conv(barx, conj(otf)), DoesNotExist()
+        return zero(eltype(rec)), zero(eltype(rec)), conv(barx, conj(otf)), zero(eltype(rec))
     end
     return Y, conv_aux_pullback
 end
-#= @adjoint conv_aux(conv, rec, otf) = begin =#
-#=     ∇ = (c -> (-1, conv(c, conj(otf)), -1)) =#
-#=     value = conv_aux(conv, rec, otf) =# 
-#=     return (value, ∇) =#
-#= end =#
