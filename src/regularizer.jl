3121d4889d2607bbb7c7526e5f89b455946a8937
roflmaostc <f.wechsler@tum.de>
2020-11-03 12:26:25 +0100

    Renaming to num_dim to num_dims in regularizer

--- src/regularizer.jl
+++ src/regularizer.jl
@@ -15,10 +15,10 @@ export generate_spatial_grad_square, generate_GR, generate_TV
 
 
 """
-    generate_indices(num_dim, d, ind1, ind2)
+    generate_indices(num_dims, d, ind1, ind2)
 Generates a list of symbols which can be used to generate Tullio expressions
 via metaprogramming.
-`num_dim` is the total number of dimensions.
+`num_dims` is the total number of dimensions.
 `d` is the dimension where there is a offset in the index.
 `ind1` and `ind2` are the offsets each.
  # Examples
@@ -27,17 +27,17 @@ julia> a, b = generate_indices(5, 2, 1, 1)
 (Any[:i1, :(i2 + 1), :i3, :i4, :i5], Any[:i1, :(i2 + 1), :i3, :i4, :i5])
 ```
 """
-function generate_indices(num_dim, d, ind1, ind2)
+function generate_indices(num_dims, d, ind1, ind2)
     # create initial symbol
     ind = :i
     # create the array of symbols for each dimension
-    inds1 = map(1:num_dim) do di
+    inds1 = map(1:num_dims) do di
         # map over numbers and append the number of the position to symbol i
         i = Symbol(ind, di)
         # at the dimension where we want to do the step, add $ind1
         di == d ? :($i + $ind1) : i
     end
-    inds2 = map(1:num_dim) do di
+    inds2 = map(1:num_dims) do di
         i = Symbol(ind, di)
         # here we do the step but in the other dimension. ind2 should be 
         # (-1) * ind1 or negative
@@ -48,13 +48,13 @@ end
 
 
 """
-    generate_laplace(num_dim, sum_dims_arr, weights)
+    generate_laplace(num_dims, sum_dims_arr, weights)
 Generate the Tullio statement for computing the Laplacian.
-`num_dim` is the dimension of the array. 
+`num_dims` is the dimension of the array. 
 `sum_dims_arr` is a array indicating over which dimensions we must sum over.
 `weights` is a array of a weight for the different dimension.
 """
-function generate_laplace(num_dim, sum_dims_arr, weights)
+function generate_laplace(num_dims, sum_dims_arr, weights)
     # create out list for the final expression
     # add accumulates the different add expressions
     out, add = [], []
@@ -62,17 +62,17 @@ function generate_laplace(num_dim, sum_dims_arr, weights)
     # be a weight provided
     for (d, w) in zip(sum_dims_arr, weights)
         # get the two lists of indices
-        inds1, inds2 = generate_indices(num_dim, d, 1, -1) 
+        inds1, inds2 = generate_indices(num_dims, d, 1, -1) 
         # critical part where we actually add the two expressions for
         # the steps in the dimension to the add array
         push!(add, :($w * arr[$(inds1...)] + $w * arr[$(inds2...)]))
     end
     # for laplace we need one final negative term at the position itself
-    inds = map(1:num_dim) do di
+    inds = map(1:num_dims) do di
         i = Symbol(:i, di)
     end
     # subtract this final term
-    pre_factor = 2 ^ num_dim
+    pre_factor = 2 ^ num_dims
     push!(add, :(-$:($pre_factor * arr[$(inds...)])))
     # create final expressions by adding all elements of the add list
     push!(out, :(@tullio threads=false res = abs2(+$(add...))))
@@ -81,20 +81,20 @@ end
 
 
 """
-    create_Ndim_regularizer(expr, num_dim, sum_dims_arr, weights, ind1, ind2)
+    create_Ndim_regularizer(expr, num_dims, sum_dims_arr, weights, ind1, ind2)
     A helper function to create a N-dimensional regularizer. In principle
     the same as `generate_laplace` but more general
     `expr` needs to be a function which takes `inds1`, `inds2` and a weight `w`-
-    `num_dim` is the total amount of dimensions
+    `num_dims` is the total amount of dimensions
     `sum_dims_arr` is a array indicating over which dimensions we must sum over.
     `weights` is a array of a weight for the different dimension.
     ``
 """
-function create_Ndim_regularizer(expr, num_dim, sum_dims_arr, weights, 
+function create_Ndim_regularizer(expr, num_dims, sum_dims_arr, weights, 
                                  ind1, ind2)
     out, add = [], []
     for (d, w) in zip(sum_dims_arr, weights)
-        inds1, inds2 = generate_indices(num_dim, d, ind1, ind2) 
+        inds1, inds2 = generate_indices(num_dims, d, ind1, ind2) 
         push!(add, expr(inds1, inds2, w))
     end
     push!(out, :(@tullio threads=false res = +($(add...))))
@@ -103,17 +103,17 @@ end
 
 
 """ 
-    generate_spatial_grad_square(num_dim, sum_dims_arr, weights)
+    generate_spatial_grad_square(num_dims, sum_dims_arr, weights)
 Generate the Tullio statement for calculating the squared spatial gradient
 over n dimensions.
-`num_dim` is the dimension of the array. 
+`num_dims` is the dimension of the array. 
 `sum_dims_arr` is a array indicating over which dimensions we must sum over.
 `weights` is a array of a weight for the different dimension.
 `ind1` and `ind2` are the offsets for the difference.
 """
-function generate_spatial_grad_square(num_dim, sum_dims_arr, weights)
+function generate_spatial_grad_square(num_dims, sum_dims_arr, weights)
     expr(inds1, inds2, w) = :($w * abs2(arr[$(inds1...)] - arr[$(inds2...)]))
-    @eval x = arr -> ($(create_Ndim_regularizer(expr, num_dim, sum_dims_arr, 
+    @eval x = arr -> ($(create_Ndim_regularizer(expr, num_dims, sum_dims_arr, 
                         weights, 1, -1)...))
     return x
 end
@@ -124,7 +124,7 @@ end
 This function returns a function to calculate the Tikhonov regularizer
 of a n-dimensional array. 
 # Arguments
-- `num_dim=2`: 
+- `num_dims=2`: 
 - `sum_dims=[1, 2]`: A array containing the dimensions we want to sum over
 - `weights=[1, 1]`: A array containing weights to weight the contribution of 
     different dimensions
@@ -135,19 +135,19 @@ of a n-dimensional array.
 To create a regularizer for a 3D dataset where the third dimension
 has different contribution.
 ```julia-repl
-julia> reg = Tikhonov(num_dim=2, sum_dims=[1, 2], weights=[1, 1], mode="identity")
+julia> reg = Tikhonov(num_dims=2, sum_dims=[1, 2], weights=[1, 1], mode="identity")
 #97 (generic function with 1 method)
 
 julia> reg([1 2 3; 4 5 6; 7 8 9])
 285
 ```
 """
-function Tikhonov(;num_dim=2, sum_dims=[1, 2], weights=[1, 1], step=1, mode="laplace")
+function Tikhonov(;num_dims=2, sum_dims=[1, 2], weights=[1, 1], step=1, mode="laplace")
     if mode == "laplace"
-        Γ = @eval arr -> ($(generate_laplace(num_dim, sum_dims, weights)...))
+        Γ = @eval arr -> ($(generate_laplace(num_dims, sum_dims, weights)...))
     elseif mode == "spatial_grad_square"
         expr(inds1, inds2, w) = :($w * abs2(arr[$(inds1...)] - arr[$(inds2...)]))
-        Γ = @eval arr -> ($(create_Ndim_regularizer(expr, num_dim, sum_dims, 
+        Γ = @eval arr -> ($(create_Ndim_regularizer(expr, num_dims, sum_dims, 
                             weights, step, (-1) * step)...))
     elseif mode == "identity"
         Γ = arr -> sum(abs2.(arr))
@@ -161,21 +161,21 @@ end
 
 
 """
-    generate_GR(num_dim, sum_dims_arr, weights, ind1, ind2, ϵ)
+    generate_GR(num_dims, sum_dims_arr, weights, ind1, ind2, ϵ)
 Generate the Tullio statement for computing the Good's roughness.
-`num_dim` is the dimension of the array. `sum_dims_arr` is a array
+`num_dims` is the dimension of the array. `sum_dims_arr` is a array
 indicating over which dimensions we must sum over.
 `weights` is a array of a weight for the different dimension.
 `ind1` and `ind2` are the offsets for the difference.
 """
-function generate_GR(num_dim, sum_dims_arr, weights, ind1, ind2)
+function generate_GR(num_dims, sum_dims_arr, weights, ind1, ind2)
     out, add = [], []
-    inds = map(1:num_dim) do di
+    inds = map(1:num_dims) do di
         i = Symbol(:i, di)
     end
     
     for (d, w) in zip(sum_dims_arr, weights)
-        inds1, inds2 = generate_indices(num_dim, d, ind1, ind2) 
+        inds1, inds2 = generate_indices(num_dims, d, ind1, ind2) 
         push!(add, :($w * (arr[$(inds1...)] + arr[$(inds2...)])))
     end
     prefactor = - 4 / (abs(ind1) + abs(ind2))
@@ -191,7 +191,7 @@ end
 This function returns a function to calculate the Good's roughness regularizer
 of a n-dimensional array. 
 # Arguments
-- `num_dim=2`: Dimension of the array that should be regularized 
+- `num_dims=2`: Dimension of the array that should be regularized 
 - `sum_dims=[1, 2]`: A array containing the dimensions we want to sum over
 - `weights=[1, 1]`: A array containing weights to weight the contribution of 
     different dimensions
@@ -202,20 +202,20 @@ of a n-dimensional array.
 To create a regularizer for a 3D dataset where the third dimension
 has different contribution. For the derivative we use forward mode.
 ```julia-repl
-julia> reg = GR(num_dim=2, sum_dims=[1, 2], weights=[1, 1], mode="forward")
+julia> reg = GR(num_dims=2, sum_dims=[1, 2], weights=[1, 1], mode="forward")
 ... (generic function with 1 method)
 
 julia> reg([1 2 3; 4 5 6; 7 8 9])
 -26.36561871738898
 ```
 """
-function GR(; num_dim=2, sum_dims=[1, 2], weights=[1, 1], step=1,
+function GR(; num_dims=2, sum_dims=[1, 2], weights=[1, 1], step=1,
               mode="central", ϵ=1f-8)
     if mode == "central"
-        GRf = @eval arr -> ($(generate_GR(num_dim, sum_dims, weights,
+        GRf = @eval arr -> ($(generate_GR(num_dims, sum_dims, weights,
                                         step, (-1) * step)...))
     elseif mode == "forward"
-        GRf = @eval arr -> ($(generate_GR(num_dim, sum_dims, weights,
+        GRf = @eval arr -> ($(generate_GR(num_dims, sum_dims, weights,
                                         step, 0)...))
     else
         throw(ArgumentError("The provided mode is not valid."))
@@ -227,9 +227,9 @@ end
 
 
 """
-    generate_TV(num_dim, sum_dims_arr, weights, ind1, ind2, ϵ)
+    generate_TV(num_dims, sum_dims_arr, weights, ind1, ind2, ϵ)
 Generate the Tullio statement for computing the Good's roughness.
-`num_dim` is the dimension of the array. 
+`num_dims` is the dimension of the array. 
 `sum_dims_arr` is a array
 indicating over which dimensions we must sum over.
 `weights` is a array of a weight for the different dimension.
@@ -237,10 +237,10 @@ indicating over which dimensions we must sum over.
 `ϵ` is a numerical constant to prevent division by zero. 
     this is important for the gradient 
 """
-function generate_TV(num_dim, sum_dims_arr, weights, ind1, ind2, ϵ=1f-8)
+function generate_TV(num_dims, sum_dims_arr, weights, ind1, ind2, ϵ=1f-8)
     out, add = [], []
     for (d, w) in zip(sum_dims_arr, weights)
-        inds1, inds2 = generate_indices(num_dim, d, ind1, ind2) 
+        inds1, inds2 = generate_indices(num_dims, d, ind1, ind2) 
         push!(add, :($w * abs2(arr[$(inds1...)] - arr[$(inds2...)])))
     end
     push!(add, ϵ)
@@ -254,7 +254,7 @@ end
 This function returns a function to calculate the Total Variation regularizer
 of a n-dimensional array. 
 # Arguments
-- `num_dim=2`: 
+- `num_dims=2`: 
 - `sum_dims=[1, 2]`: A array containing the dimensions we want to sum over
 - `weights=[1, 1]`: A array containing weights to weight the contribution of 
     different dimensions
@@ -265,20 +265,20 @@ of a n-dimensional array.
 To create a regularizer for a 3D dataset where the third dimension
 has different contribution. For the derivative we use forward mode.
 ```julia-repl
-julia> reg = TV(num_dim=2, sum_dims=[1, 2], weights=[1, 1], mode="forward")
+julia> reg = TV(num_dims=2, sum_dims=[1, 2], weights=[1, 1], mode="forward")
 ... (generic function with 1 method)
 
 julia> reg([1 2 3; 4 5 6; 7 8 9])
 12.649111f0
 ```
 """
-function TV(; num_dim=2, sum_dims=[1, 2], weights=[1, 1], step=1, mode="central")
+function TV(; num_dims=2, sum_dims=[1, 2], weights=[1, 1], step=1, mode="central")
 
     if mode == "central"
-        total_var = @eval arr -> ($(generate_TV(num_dim, sum_dims, weights,
+        total_var = @eval arr -> ($(generate_TV(num_dims, sum_dims, weights,
                                         step, (-1) * step)...))
     elseif mode == "forward"
-        total_var = @eval arr -> ($(generate_TV(num_dim, sum_dims, weights,
+        total_var = @eval arr -> ($(generate_TV(num_dims, sum_dims, weights,
                                         step, 0)...))
     else
         throw(ArgumentError("The provided mode is not valid."))
