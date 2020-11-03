d2f9817354989b66c6dc4c60102d390ec18b7ada
roflmaostc <f.wechsler@tum.de>
2020-11-03 12:25:44 +0100

    Add test cases for lossfunctions

--- test/lossfunctions.jl
+++ test/lossfunctions.jl
@@ -13,6 +13,10 @@ end
 @testset "Gaussian Loss" begin
     N = 6
     img = abs.(randn((N, N)))
+
+    gauss = Gauss()
+    @test 0 ≈ gauss(img, img)
+
     @test 0 ≈ gauss_aux(img, img)
     
     @test all(0 .≈ Zygote.gradient(gauss_aux, img, img)[1])
@@ -24,6 +28,9 @@ end
     N = 6
     img = abs.(randn((N, N)))
     
+    scaled_gauss = ScaledGauss()
+    
+    @test 1.7047189562170502 ≈ scaled_gauss([5.], [2.]) 
     @test 1.7047189562170502 ≈ scaled_gauss_aux([5.], [2.]) 
     
     @test all( -1 .≈ Zygote.gradient(scaled_gauss_aux, [1.], [2.])[1])
