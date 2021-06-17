0dc692d877ecefa4ff165fd0f31205dda7814755
roflmaostc <fxw+git@mailbox.org>
2021-06-17 16:43:11 +0200

    Add in place loss functions

--- src/lossfunctions.jl
+++ src/lossfunctions.jl
@@ -10,24 +10,25 @@ Calculates the Poisson loss for `μ` and `meas`.
 `μ` can be of larger size than `meas`. In that case
 we extract a centered region from `μ` of the same size as `meas`.
 """ 
-function poisson_aux(μ, meas)
+function poisson_aux(μ, meas, storage=copy(μ))
     # due to numerical errors, μ can be negative or 0
     if minimum(μ) <= 0
         μ .+= eps(maximum(μ)) .+ abs.(minimum(μ))
     end
-    return sum(μ .- meas .* log.(μ))
+    storage .= μ .- meas .* log.(μ)
+    return sum(storage)
 end
 
 
  # define custom gradient for speed-up
  # ChainRulesCore offers the possibility to define a backward AD rule
  # which can be used by several different AD systems
-function ChainRulesCore.rrule(::typeof(poisson_aux), μ, meas)
-    Y = poisson_aux(μ, meas)
+function ChainRulesCore.rrule(::typeof(poisson_aux), μ, meas, storage=copy(μ))
+    Y = poisson_aux(μ, meas, storage)
 
     function poisson_aux_pullback(xbar)
-        ∇ = xbar .* (one(eltype(μ)) .- meas ./ μ)
-        return zero(eltype(μ)), ∇, zero(eltype(μ)) 
+        storage .= xbar .* (one(eltype(μ)) .- meas ./ μ)
+        return zero(eltype(μ)), storage, zero(eltype(μ)), zero(eltype(storage)) 
     end
 
     return Y, poisson_aux_pullback
@@ -53,12 +54,13 @@ Calculates the Gauss loss for `μ` and `meas`.
 `μ` can be of larger size than `meas`. In that case
 we extract a centered region from `μ` of the same size as `meas`.
 """ 
-function gauss_aux(μ, meas)
-    return sum(abs2.(μ - meas))
+function gauss_aux(μ, meas, storage=copy(μ))
+    storage .= abs2.(μ - meas)
+    return sum(storage)
 end
 
  # define custom gradient for speed-up
-function ChainRulesCore.rrule(::typeof(gauss_aux), μ, meas)
+function ChainRulesCore.rrule(::typeof(gauss_aux), μ, meas, storage=copy(μ))
     Y = gauss_aux(μ, meas) 
     function gauss_aux_pullback(xbar)
         return zero(eltype(μ)), 2 .* (μ - meas), zero(eltype(μ)) 
@@ -87,13 +89,14 @@ Calculates the scaled Gauss loss for `μ` and `meas`.
 `μ` can be of larger size than `meas`. In that case
 we extract a centered region from `μ` of the same size as `meas`.
 """
-function scaled_gauss_aux(μ, meas)
+function scaled_gauss_aux(μ, meas, storage=copy(μ))
     μ[μ .<= 1f-8] .= 1f-8
-    return sum(1/2f0 .* log.(μ) .+ (meas .- μ).^2 ./ (2 .* μ))
+    storage .= 1/2f0 .* log.(μ) .+ (meas .- μ).^2 ./ (2 .* μ)
+    return sum(storage)
 end
 
  # define custom gradient for speed-up
-function ChainRulesCore.rrule(::typeof(scaled_gauss_aux), μ, meas)
+function ChainRulesCore.rrule(::typeof(scaled_gauss_aux), μ, meas, storage=copy(μ))
     Y = scaled_gauss_aux(μ, meas) 
     function scaled_gauss_aux_pullback(xbar)
         ∇ = (μ + μ.^2 - meas.^2)./(2 .* μ.^2)
